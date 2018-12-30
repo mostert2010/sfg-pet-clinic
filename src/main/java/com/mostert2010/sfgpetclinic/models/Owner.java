5a9aaa294be16e0758368e203ae3069236f2a320
Wessel Mostert <mostert2010@gmail.com>
2018-12-30 14:10:04 +0200

    Udemy Spring Lecture 54 - Added POJOs. Closes #2.

---
+++ src/main/java/com/mostert2010/sfgpetclinic/models/Owner.java
@@ -0,0 +1,5 @@
+package com.mostert2010.sfgpetclinic.models;
+
+public class Owner extends Person {
+
+}
---
+++ src/main/java/com/mostert2010/sfgpetclinic/models/Vet.java
@@ -0,0 +1,5 @@
+package com.mostert2010.sfgpetclinic.models;
+
+public class Vet extends Person {
+
+}
