3bd7c66e07dc8d902b878472a5da52b0c88f8d22
Wessel Mostert <mostert2010@gmail.com>
2018-12-31 23:27:42 +0200

    Udemy Spring Lecture 71 - add Index page and controller. closes #8

---
+++ pet-clinic-web/src/main/java/com/mostert2010/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package com.mostert2010.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping({"", "/", "index", "index.html"})
+	public String index() {
+		return "index";
+	}
+}
