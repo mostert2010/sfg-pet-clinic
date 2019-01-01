a4cfa70f7508c5943ee14e9cbeb4f77372aec63c
Wessel Mostert <mostert2010@gmail.com>
2019-01-01 21:19:45 +0200

    Udemy Spring Lecture 89 - Display Owners list on Index page closes #16

--- pet-clinic-web/src/main/java/com/mostert2010/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/mostert2010/sfgpetclinic/controllers/OwnerController.java
@@ -1,14 +1,28 @@
 package com.mostert2010.sfgpetclinic.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mostert2010.sfgpetclinic.services.OwnerService;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
-	@RequestMapping({"", "/", "/index", "/index.html"})
-	public String listOwners() {
+	private final OwnerService ownerService;
+
+	@Autowired
+	public OwnerController(OwnerService ownerService) {
+		this.ownerService = ownerService;
+	}
+
+	@RequestMapping({ "", "/", "/index", "/index.html" })
+	public String listOwners(Model model) {
+		
+		model.addAttribute("owners", ownerService.findAll());
+		
 		return "owners/index";
 	}
 }
