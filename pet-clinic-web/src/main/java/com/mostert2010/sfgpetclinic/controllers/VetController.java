d93274625c02be2454e5a133f2fe8c467cb0674c
Wessel Mostert <mostert2010@gmail.com>
2019-01-01 21:33:42 +0200

    Udemy Spring Lecture 90 - Display Vets list on Index page closes #15

--- pet-clinic-web/src/main/java/com/mostert2010/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/mostert2010/sfgpetclinic/controllers/VetController.java
@@ -1,14 +1,28 @@
 package com.mostert2010.sfgpetclinic.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mostert2010.sfgpetclinic.services.VetService;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
 
-	@RequestMapping({"", "/", "/index", "/index.html"})
-	public String listVets() {
+	private final VetService vetService;
+
+	@Autowired
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
+
+	@RequestMapping({ "", "/", "/index", "/index.html" })
+	public String listVets(Model model) {
+		
+		model.addAttribute("vets", vetService.findAll());
+		
 		return "vets/index";
 	}
 }
