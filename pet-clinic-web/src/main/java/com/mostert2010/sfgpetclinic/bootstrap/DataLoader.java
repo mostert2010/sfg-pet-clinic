aca2452f6933d22c6a4544cf00169cdbc6dc8a25
Wessel Mostert <mostert2010@gmail.com>
2019-01-03 20:49:33 +0200

    Udemy Spring Lecture 129 - Added PetType to bootstrap. closes #32

--- pet-clinic-web/src/main/java/com/mostert2010/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/mostert2010/sfgpetclinic/bootstrap/DataLoader.java
@@ -5,8 +5,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.mostert2010.sfgpetclinic.models.Owner;
+import com.mostert2010.sfgpetclinic.models.PetType;
 import com.mostert2010.sfgpetclinic.models.Vet;
 import com.mostert2010.sfgpetclinic.services.OwnerService;
+import com.mostert2010.sfgpetclinic.services.PetTypeService;
 import com.mostert2010.sfgpetclinic.services.VetService;
 
 @Component
@@ -14,16 +16,28 @@ public class DataLoader implements CommandLineRunner {
 
 	private final OwnerService ownerService;
 	private final VetService vetService;
+	private final PetTypeService petTypeService;
 
 	@Autowired
-	public DataLoader(OwnerService ownerService, VetService vetService) {
+	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
 		this.ownerService = ownerService;
 		this.vetService = vetService;
+		this.petTypeService = petTypeService;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
 
+		PetType dog = new PetType();
+		dog.setName("Dog");
+		
+		PetType savedDogPetType = petTypeService.save(dog);
+		
+		PetType cat = new PetType();
+		cat.setName("Cat");
+		
+		PetType savedCatPetType = petTypeService.save(cat);
+		
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Michael");
 		owner1.setLastName("Weston");
