1f0884bb140b7616923b366a9efd6ea39f7ba773
Wessel Mostert <mostert2010@gmail.com>
2019-01-03 19:00:54 +0200

    Udemy Spring Lecture 125 - added Visit entity. closes #26

--- pet-clinic-data/src/main/java/com/mostert2010/sfgpetclinic/models/Owner.java
+++ pet-clinic-data/src/main/java/com/mostert2010/sfgpetclinic/models/Owner.java
@@ -1,5 +1,18 @@
 package com.mostert2010.sfgpetclinic.models;
 
+import java.util.Set;
+
 public class Owner extends Person {
 
+	Set<Pet> pets;
+
+	public Set<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
+	}
+	
+	
 }
