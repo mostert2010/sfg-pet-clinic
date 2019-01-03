2b109bf157af3bf6eb9e2c25fb7d2781ea2c3020
Wessel Mostert <mostert2010@gmail.com>
2019-01-03 19:06:33 +0200

    Udemy Spring Lecture 126 - added Specialty entity. closes #27

---
+++ pet-clinic-data/src/main/java/com/mostert2010/sfgpetclinic/models/Specialty.java
@@ -0,0 +1,16 @@
+package com.mostert2010.sfgpetclinic.models;
+
+public class Specialty extends BaseEntity {
+
+	private String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
